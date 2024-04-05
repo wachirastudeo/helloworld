b8bd682559aa38632931f15b81caa8b4a30b2760
wachirastudeo <wachirastudeo@gmail.com>
2024-04-05 19:57:51 +0700

    test go get echo

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/wachirastudeo/helloworld/calculator"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	fmt.Println(calculator.Add(1, 2))
-
+	// fmt.Println(calculator.Add(1, 2))
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
