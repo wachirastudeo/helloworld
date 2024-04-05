1faffdd52e38c6e11d6be1d7ef45e1da7cdcd740
wachirastudeo <wachirastudeo@gmail.com>
2024-04-05 20:01:14 +0700

    test user variable

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
+	"fmt"
 )
 
 func main() {
 	// fmt.Println(calculator.Add(1, 2))
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	var a int = 10
+	fmt.Println(a)
+	b := 20
+	fmt.Println(b)
+
 }
